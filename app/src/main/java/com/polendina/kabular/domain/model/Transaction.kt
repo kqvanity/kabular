694cdda7f9ea90e983909f02dc3bd01a0c32aac4
mostafa mahmoud <solicitmind@gmail.com>
2024-03-28 18:12:09 +0200

    feat: append handy extension functions for text and numerals

--- app/src/main/java/com/polendina/kabular/domain/model/Transaction.kt
+++ app/src/main/java/com/polendina/kabular/domain/model/Transaction.kt
@@ -14,7 +14,7 @@ data class Transaction(
     val profit: Long
         get() = earnings - expenditure
     val growthProportion: Double
-        get() = (earnings.toDouble() / expenditure).roundTo(2)
+        get() = (earnings.toDouble() / if (expenditure == 0L) 1 else expenditure).roundTo(2)
     val loss: Boolean
         get() = profit < 0
 }
--- app/src/main/java/com/polendina/kabular/utils/textual.kt
+++ app/src/main/java/com/polendina/kabular/utils/textual.kt
@@ -1,3 +1,6 @@
 package com.polendina.kabular.utils
 
 fun String.isNotEmptyNorBlank(): Boolean = isNotEmpty() || isNotBlank()
+
+// TODO: I think this extension should be tested out. Kinda sussy
+fun String.initialCapital(): String = if (isEmpty()) "" else first().uppercase().plus(substring(1 until length).lowercase())
